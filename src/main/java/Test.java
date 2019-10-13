271c784fbc7687e1fbb933d9b5c170815946c168
Alex Kononchuk <axeane@gmail.com>
2019-10-13 12:04:59 +0200

    Initial commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        printMessage("Hello World!");
+    }
+
+    private static void printMessage(String text) {
+        System.out.println(text);
+    }
+}
