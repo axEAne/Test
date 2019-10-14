f8f3335f5accea01cb9f680d4597fd93f031843e
Alex Kononchuk <axeane@gmail.com>
2019-10-14 18:24:56 +0200

    Edited BaseEntity

--- src/main/java/BaseEntity.java
+++ src/main/java/BaseEntity.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
-
 public class BaseEntity {
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseEntity{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
